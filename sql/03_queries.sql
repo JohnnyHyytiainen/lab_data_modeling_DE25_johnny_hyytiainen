8f77e5a4d8daf16fef5b7ea2826efaa8874e0cb0
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2026-01-20 14:34:42 +0100

    feat(queries MvP update): updated queries script with updated comments and separations in cli output to avoid confusion

--- sql/03_queries.sql
+++ sql/03_queries.sql
@@ -1,5 +1,12 @@
--- 1) Studenter + klass + program + ort
--- INNER + LEFT JOIN
+-- ==========================================
+-- Q-1A. Student overview. Students + Class + Program + location(ort)
+-- JOIN(INNER) + LEFT JOIN + ORDER BY(Sorting)
+-- ==========================================
+\echo ''
+\echo '================================='
+\echo 'Query 1A: Student roster, Class, Program and Location including program classes and stand alone classes'
+\echo 'Purpose: Retrieving all students with their class, program, and campus city'
+\echo '================================='
 SELECT
   p.first_name, p.last_name, p.email,
   c.class_code,
@@ -12,7 +19,15 @@ JOIN facility f ON f.facility_id = c.facility_id
 LEFT JOIN program pr ON pr.program_id = c.program_id
 ORDER BY c.class_code, p.last_name;
 
--- 1.5) Studenter + klass + program + ort med enbart DE25-01 som klasskod
+-- ==========================================
+-- Q-1B. Student overview. Student + Class + Program + Location(ort) filtered by Class Code DE25-01
+-- JOIN(INNER) + WHERE(Filtering)
+-- ==========================================
+\echo ''
+\echo '================================='
+\echo 'Query 1B: Student roster, Program, Class and city'
+\echo 'Purpose: Retrieving all students with their class tied to a specific program and which campus'
+\echo '================================='
 SELECT
   p.first_name, p.last_name, p.email,
   c.class_code,
@@ -27,9 +42,15 @@ WHERE c.class_code = 'DE25-01'
 ORDER BY c.class_code, p.last_name;
 
 
--- 2) Utbildare och om de är konsult + företag (POSTGRESQL variant)
--- INNER + LEFT JOIN
-\echo '----- Query 2) Educators + consultant status (IS NOT NULL) -----'
+-- ==========================================
+-- Q-2A. Educator, Consultant and Company query (POSTGRESQL SPECIFIC variant) 
+-- JOIN(INNER) + LEFT JOIN
+-- ==========================================
+\echo ''
+\echo '================================='
+\echo 'Query 2A: Educator status (Consultants + Employees)'
+\echo 'Purpose: Identifying consultants using Boolean logic (IS NOT NULL, POSTGRESQL SPECIFIC variant)'
+\echo '================================='
 SELECT
   p.first_name, p.last_name, e.competence_area,
   (co.person_id IS NOT NULL) AS is_consultant,
@@ -41,9 +62,16 @@ LEFT JOIN consultant co ON co.person_id = e.person_id
 LEFT JOIN consultant_company cc ON cc.consultant_company_id = co.consultant_company_id
 ORDER BY is_consultant DESC, p.last_name;
 
--- 2.5) Utbildare och om de är konsult + företag (Standard SQL CASE WHEN .... ELSE .... END variant)
--- INNER + LEFT JOIN
-\echo '----- Query 2.5) Educators + consultant status (CASE WHEN) -----'
+-- ==========================================
+-- Q-2B. Educator, Consultant and Company query (STANDARD SQL variant using CASE WHEN .... ELSE.... END)
+-- JOIN(INNER) + LEFT JOIN + Sorting(ORDER BY)
+-- ==========================================
+\echo ''
+\echo '================================='
+\echo 'Query 2B: Educator status (Consultants + Employees)'
+\echo 'Purpose: Identifying consultants using Boolean logic (CASE WHEN..ELSE..END.. STANDARD SQL variant)'
+\echo '================================='
+
 SELECT
   p.first_name,
   p.last_name,
@@ -60,8 +88,15 @@ LEFT JOIN consultant co ON co.person_id = e.person_id
 LEFT JOIN consultant_company cc ON cc.consultant_company_id = co.consultant_company_id
 ORDER BY is_consultant DESC, p.last_name;
 
--- 3) Program → kurser (termin)
--- INNER JOIN
+-- ==========================================
+-- Q-3. Progran(program code), Semester nr, Course related query(prog_code, sem_nr, course_code/name/points)
+-- JOIN(INNER) + Sorting(ORDER BY)
+-- ==========================================
+\echo ''
+\echo '================================='
+\echo 'Query 3: Program status (Program code, Semester nr, Course code, name and points'
+\echo 'Purpose: Identifying relevant Course information tied to a Program'
+\echo '================================='
 SELECT
   pr.program_code,
   pc.semester_number,
@@ -73,8 +108,15 @@ JOIN program pr ON pr.program_id = pc.program_id
 JOIN course cr ON cr.course_id = pc.course_id
 ORDER BY pr.program_code, pc.semester_number;
 
--- 4) Fristående kurser (kurser som inte ingår i något program)
--- LEFT JOIN
+-- ==========================================
+-- Q-4. Stand alone courses(Fristående kurser) not part of any program.
+-- LEFT JOIN + Searching for NULL VALUE(NULL value = not tied to specific program) + Sorting(ORDER BY)
+-- ==========================================
+\echo ''
+\echo '================================='
+\echo 'Query 4: Standalone Courses(Freestanding Courses)'
+\echo 'Purpose: Identifying courses not tied to a specific Program. (NULL VALUE == Standalone Course)'
+\echo '================================='
 SELECT cr.course_code, 
 cr.course_name
 FROM course cr
@@ -82,9 +124,18 @@ LEFT JOIN program_course pc ON pc.course_id = cr.course_id
 WHERE pc.course_id IS NULL
 ORDER BY cr.course_code;
 
-
--- 5) Utb ledare/klass admin har hand om klasser (visar admins klasser)
--- INNER JOIN
+-- ==========================================
+-- Q-5A Searching for Employee Class administrators and what programs + classes they are admins for.
+-- JOIN(INNER) + GROUPING + Sorting(ORDER BY)
+-- ==========================================
+\echo ''
+\echo '================================='
+\echo 'Query 5A: Class Administrator TOTAL workload'
+\echo 'Purpose: Identifying number of TOTAL CLASSES managed by a Class admin'
+\echo '================================='
+\echo 'Business ambiguity identified. Requirements did not specify LIMIT to numbers of classes managed by admin'
+\echo 'Recommendation: Review wording in business requirements to not overload Class admins with too much work!'
+\echo '================================='
 SELECT
   p.first_name, 
   p.last_name, 
@@ -96,9 +147,18 @@ JOIN person p ON p.person_id = emp.person_id
 GROUP BY p.first_name, p.last_name, emp.employee_nr
 ORDER BY classes_managed DESC, emp.employee_nr;
 
--- 5.5) Utb ledare/klass admin har hand om 3 klasser (räkna klasser per admin)
--- NOTE i seed datan har Alex hand om 4 klasser(inklusive fristående FREE-01)
-\echo '----- Query 5.5) Alex is class manager for 3 PROGRAMS and 1 Standalone course. -----'
+-- ==========================================
+-- Q-5B Searching for Employee Class administrators to see how many CLASSES they are administrators for.
+-- JOIN(INNER) + Grouping(GROUP BY), Counting(HAVING COUNT), Ordering(ORDER BY)
+-- ==========================================
+\echo ''
+\echo '================================='
+\echo 'Query 5B: Class Administrator TOTAL workload'
+\echo 'Purpose: Identifying number of TOTAL CLASSES managed by a Class admin'
+\echo '================================='
+\echo 'Alex is currently managing 3 Programs and 1 Standalone Course'
+\echo 'Recommendation: See prior recommendation for business requirements ambiguity'
+\echo '================================='
 SELECT
   p.first_name, p.last_name, emp.employee_nr,
   COUNT(*) AS classes_managed
@@ -109,8 +169,16 @@ GROUP BY p.first_name, p.last_name, emp.employee_nr
 HAVING COUNT(*) >= 3
 ORDER BY classes_managed DESC, emp.employee_nr;
 
--- 5.6) Utb ledare/klass admin för PROGRAM klasser (ignorera fristående kurser)
-\echo '----- Query 5.6) query to ONLY show PROGRAM admins. -----'
+-- ==========================================
+-- Q-5C Searching for how many SPECIFIC PROGRAMS one Employee is a Class Administrator for. Ignoring Stand alone courses
+-- JOIN(INNER) + Ignoring NULL VALUES(stand alone = NULL) + WHERE(Filtering) + GROUP BY(Sorting)
+-- ==========================================
+\echo ''
+\echo '================================='
+\echo 'Query 5C: Class Administrator TOTAL Program workload'
+\echo 'Purpose: Identifying total nr of PROGRAMS Administered by Class Admin'
+\echo 'Alex is currently Administrating the MAXIMUM number of Programs.'
+\echo '================================='
 SELECT
   p.first_name, p.last_name, emp.employee_nr,
   COUNT(*) AS program_classes_managed
@@ -121,9 +189,15 @@ WHERE c.program_id IS NOT NULL
 GROUP BY p.first_name, p.last_name, emp.employee_nr
 HAVING COUNT(*) = 3;
 
-
--- 6) Teaching assignments(undervisningstillfällen) per klass. Vilka utbildare undervisar vilka kurser
--- INNER JOIN
+-- ==========================================
+-- Q-6A Searching for teaching assignments per class. Which Educator(teacher) teaches What Courses
+-- JOIN(INNER) + ORDER BY(Sorting)
+-- ==========================================
+\echo ''
+\echo '================================='
+\echo 'Query 6A: Teaching Assignments'
+\echo 'Purpose: Linking the Class, Course and Program to When'
+\echo '================================='
 SELECT
   c.class_code,
   cr.course_code,
@@ -137,8 +211,15 @@ JOIN course cr ON cr.course_id = ta.course_id
 JOIN person p ON p.person_id = ta.educator_person_id
 ORDER BY c.class_code, ta.start_date;
 
-
--- 6.5) Teaching assignments(undervisningstillfällen). Antal kurser
+-- ==========================================
+-- Q-6B Searching for Teaching Assignments. Which class and how many courses
+-- JOIN(INNER) + GROUP BY, ORDER BY(Sorting)
+-- ==========================================
+\echo ''
+\echo '================================='
+\echo 'Query 6B: Teachin Assignments'
+\echo 'Purpose: Linking number of Courses a Specific Program or Class has.'
+\echo '================================='
 SELECT 
   c.class_code, 
   COUNT(DISTINCT ta.course_id) AS num_courses
